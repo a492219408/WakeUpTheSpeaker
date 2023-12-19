bb13c445bfc5415fb641ee5b5340bb3eca469c80
YmdElf <a492219408@163.com>
2023-12-19 15:22:45 +0800

    修复

--- WakeUpTheSpeaker/WakeUpTheSpeaker.cpp
+++ WakeUpTheSpeaker/WakeUpTheSpeaker.cpp
@@ -1,7 +1,8 @@
 ﻿// WakeUpTheSpeaker.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 
 #include <windows.h>
-#include <mmsystem.h>
+#include <mmdeviceapi.h>
+#include <endpointvolume.h>
 
 /**
  * \brief 
@@ -13,22 +14,39 @@
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-	DWORD old_volume;
+    CoInitialize(nullptr);
+    IMMDeviceEnumerator* device_enumerator = nullptr;
+    CLSID clsid;
+    CLSIDFromString(L"{BCDE0395-E52F-467C-8E3D-C4579291692E}", &clsid); // UUID for MMDeviceEnumerator
+    // CoCreateInstance(__uuidof(MMDeviceEnumerator), nullptr, CLSCTX_INPROC_SERVER, __uuidof(IMMDeviceEnumerator), reinterpret_cast<LPVOID*>(&device_enumerator));
+    CoCreateInstance(clsid, nullptr, CLSCTX_INPROC_SERVER, __uuidof(IMMDeviceEnumerator), reinterpret_cast<LPVOID*>(&device_enumerator));
+    IMMDevice* default_device = nullptr;
+    device_enumerator->GetDefaultAudioEndpoint(eRender, eConsole, &default_device);
+    device_enumerator->Release();
+    device_enumerator = nullptr;
 
-	// 获取当前音量
-	waveOutGetVolume(nullptr, &old_volume);
+    IAudioEndpointVolume* endpoint_volume = nullptr;
+    CLSIDFromString(L"{5CDF2C82-841E-4546-9722-0CF74078229A}", &clsid); // UUID for IAudioEndpointVolume
+    // default_device->Activate(__uuidof(IAudioEndpointVolume), CLSCTX_INPROC_SERVER, nullptr, reinterpret_cast<LPVOID*>(&endpoint_volume));
+    default_device->Activate(clsid, CLSCTX_INPROC_SERVER, nullptr, reinterpret_cast<LPVOID*>(&endpoint_volume));
+    default_device->Release();
+    default_device = nullptr;
 
-	// 设置音量为30%
-	constexpr DWORD new_volume = static_cast<DWORD>((static_cast<double>(30) / 100) * 0xFFFF) + (static_cast<DWORD>((
-		static_cast<double>(30) / 100) * 0xFFFF) << 16);
-	// 设置音量为30%
-	waveOutSetVolume(nullptr, new_volume);
+    float current_volume = 0;
+    endpoint_volume->GetMasterVolumeLevelScalar(&current_volume);
 
-	// 播放声音
-	Beep(500, 300);
+    // Set volume to 30%
+    endpoint_volume->SetMasterVolumeLevelScalar(static_cast<float>(0.3), nullptr);
 
-	// 恢复原来的音量
-	waveOutSetVolume(nullptr, old_volume);
+    // Play sound
+    Beep(500, 300);
+
+    // Restore original volume
+    endpoint_volume->SetMasterVolumeLevelScalar(current_volume, nullptr);
+
+    endpoint_volume->Release();
+
+    CoUninitialize();
 
 	return 0;
 }
